d18d902589fbd4b72a188da16ba050d70c9ddb26
Ville Sillanpää <ville.sillanpaa@futurice.com>
2017-09-27 16:07:10 +0300

    fetch company icons and the map blueprint from the server

--- src/modules/views/MapView.js
+++ src/modules/views/MapView.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import {
   Image,
   Text,
@@ -11,81 +10,105 @@ import {
 } from 'react-native';
 
 import AppStyles from '../AppStyles';
-
 import { apiRoot } from '../../utils/rest';
-
 import { getTranslated, texts } from '../../utils/translation';
+import { connect } from 'react-redux';
+import rest from '../../utils/rest';
+
+const mapStateToProps = state => ({
+  companies: state.companies.data,
+});
+const mapDispatchToProps = dispatch => ({
+  refresh: () => dispatch(rest.actions.companies()),
+});
 
 export class MapView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      source: { uri: `${apiRoot}/public/map_template.png` },
+    };
+  }
   static navigationOptions = {
     title: getTranslated(texts.mapTitle),
   };
 
+  handleOnLayout = evt => {
+    const { layout } = evt.nativeEvent;
+    const containerWidth = layout.width;
+
+    Image.getSize(this.state.source, (width, height) => {
+      this.setState({
+        mapImageWidth: containerWidth,
+        mapImageHeight: containerWidth * height / width,
+        mapImageX: layout.x,
+        mapImageY: layout.y,
+      });
+    });
+  };
+
   render() {
-    const HTML = `
-    <!DOCTYPE html>\n
-    <html>
-      <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8">
-        <style type="text/css">
-          body {
-            margin: 0;
-            padding: 0;
-          }
-          img {
-            position: absolute;
-            margin: auto;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            max-height: 100%;
-            max-width: 100%;
-          }
-        </style>
-      </head>
-      <body>
-        <img src="${apiRoot}/public/map.png" />
-      </body>
-    </html>
-    `;
+    const { companies } = this.props;
+    const { mapImageX, mapImageY, mapImageWidth, mapImageHeight } = this.state;
+
+    var mapImageStyle = {
+      width: mapImageWidth,
+      height: mapImageHeight,
+    };
 
     return (
       <View style={styles.MapContainer}>
-        <View style={styles.statusBar} />
-        <WebView style={styles.MapImage} source={{ html: HTML }} />
+        <Image
+          style={mapImageStyle}
+          source={this.state.source}
+          ref="mapImage"
+          onLayout={this.handleOnLayout}
+        />
+        {companies.map((company, index) => {
+          const { positionX, positionY } = company;
+          var left = 0,
+            top = 0;
+
+          if (positionX > 0.0 || positionY > 0.0) {
+            left = positionX * mapImageWidth + mapImageX;
+            top = positionY * mapImageHeight + mapImageY;
+          } else {
+            //hide the company icon if it's not placed
+            left = -100;
+            top = -100;
+          }
+
+          var style = {
+            width: 26,
+            height: 26,
+            position: 'absolute',
+            left: left,
+            top: top,
+          };
+
+          return (
+            <Image
+              key={index}
+              style={style}
+              source={{
+                uri: `${apiRoot}/public/company${company.companyId}.png`,
+              }}
+              defaultSource={require('../../../images/defCompanyImg.png')}
+            />
+          );
+        })}
       </View>
     );
   }
 }
 
 let styles = StyleSheet.create({
-  statusBar: {
-    alignSelf: 'stretch',
-    backgroundColor: AppStyles.lightRed,
-    height: AppStyles.statusbarHeight,
-    justifyContent: 'center',
-  },
-  header: {
-    alignSelf: 'stretch',
-    backgroundColor: AppStyles.lightRed,
-    elevation: AppStyles.headerElevation,
-    height: AppStyles.headerHeight,
-    justifyContent: 'center',
-  },
-  headerText: {
-    textAlign: 'center',
-    color: AppStyles.white,
-    fontSize: AppStyles.headerFontSize,
-    fontWeight: 'bold',
-  },
   MapContainer: {
     flex: 1,
     backgroundColor: AppStyles.whiteBackground,
-  },
-  MapImage: {
-    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
 });
 
-export default MapView;
+export default connect(mapStateToProps, mapDispatchToProps)(MapView);
