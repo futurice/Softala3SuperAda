b758daab35e0acb2aea5b328a8d79e460f1f3ba6
Rasmus Eskola <fruitiex@gmail.com>
2018-01-05 14:52:43 +0200

    Fix config vars not being exported from config file (broke Quiz results)

--- src/modules/Config.js
+++ src/modules/Config.js
@@ -1,6 +1,6 @@
-const pointsPerMinute = 10;
-const pointsPerWord = 5;
-const pointsCompleted = 100;
-const maxMinutes = 10;
+export const pointsPerMinute = 10;
+export const pointsPerWord = 5;
+export const pointsCompleted = 100;
+export const maxMinutes = 10;
 
 export const timelimit = __DEV__ ? 100 : maxMinutes * 60;
